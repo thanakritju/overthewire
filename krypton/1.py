4d8fa99312ad7986de330106503d0d318f6decf1
thanakritju <thanakrit.ju.work@gmail.com>
2021-12-12 19:55:25 +0700

    level 1 and 2

---
+++ krypton/1.py
@@ -0,0 +1,5 @@
+import base64
+
+encoded = 'S1JZUFRPTklTR1JFQVQ='
+
+print(base64.b64decode(encoded))
---
+++ krypton/2.py
@@ -0,0 +1,19 @@
+from string import ascii_uppercase
+
+
+encrypted = 'YRIRY GJB CNFFJBEQ EBGGRA'
+rotation = 13
+answer = ""
+
+for char in encrypted:
+    if char == " ":
+        answer += " "
+    else:
+        new_index = (ascii_uppercase.index(char) + rotation) % 26
+        answer += ascii_uppercase[new_index]
+
+print(answer)
+
+
+
+
